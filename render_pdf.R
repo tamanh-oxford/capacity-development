c5ad46c6ab97d35f35a14c6c357e5ce2d3481760
Ernest Guevarra <ernest@guevarra.io>
2026-01-04 06:46:02 +0000

    update output directory to _site

--- render_pdf.R
+++ render_pdf.R
@@ -7,7 +7,7 @@ library(pagedown)
 ## Render data frameworks slides to PDF ----
 
 html_files <- list.files(
-  path = "_slides/data-frameworks", 
+  path = "_site/data-frameworks", 
   pattern = "html", 
   full.names = TRUE
 )
@@ -25,20 +25,20 @@ Map(
 ## Combine data frameworks slides into a single PDF ----
 
 pdf_files <- list.files(
-  path = "_slides/data-frameworks",
+  path = "_site/data-frameworks",
   pattern ="pdf",
   full.names = TRUE
 )
 
 qpdf::pdf_combine(
-  input = pdf_files, output = "_slides/data-frameworks/data_governance.pdf"
+  input = pdf_files, output = "_site/data-frameworks/data_governance.pdf"
 )
 
 ## Render data management slides to PDF ----
 
 pagedown::chrome_print(
-  input = "_slides/data-management/index.html",
-  output = "_slides/data-management/data_management.pdf",
+  input = "_site/data-management/index.html",
+  output = "_site/data-management/data_management.pdf",
   timeout = 300,
   extra_args = c("--no-sandbox", "--disable-dev-shm-usage")
 )
